5cf0485dcb2890f6b45c812d4c55d778d8496070
Mezni <mamezni@gmail.com>
2025-01-24 03:55:53 +0000

    commit 2025/01/24

---
+++ work/polar/src/main.rs
@@ -0,0 +1,44 @@
+use deltalake::{
+    kernel::{DataType, PrimitiveType, StructField, StructType},
+    DeltaOps, DeltaTable, DeltaTableError,
+};
+
+pub fn table_schema() -> StructType {
+    StructType::new(vec![
+        StructField::new("session_id", DataType::Primitive(PrimitiveType::String), false),
+        StructField::new("timestamp", DataType::Primitive(PrimitiveType::String), false),
+    ])
+}
+
+pub async fn get_delta_table(
+    path: &str,
+    delta_schema: StructType,
+    partitions: Option<Vec<&str>>,
+) -> Result<DeltaTable, DeltaTableError> {
+    let table = match deltalake::open_table(path).await {
+        Ok(table) => table,
+        Err(DeltaTableError::InvalidTableLocation(_)) => {
+            DeltaOps::try_from_uri(path)
+                .await?
+                .create()
+                .with_columns(delta_schema.fields().cloned().collect::<Vec<_>>())
+                .with_partition_columns(partitions.unwrap_or_default())
+                .await?
+        }
+        _ => panic!("unexpected err while reading delta_table"),
+    };
+
+    Ok(table)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), DeltaTableError> {
+    let _trips_table = get_delta_table(
+        "./delta_lake/ccccc",
+        table_schema(),
+        None,  
+    )
+    .await?;
+
+    Ok(())
+}
