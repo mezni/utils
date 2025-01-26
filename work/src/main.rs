2f89bbe07faf6add4ddc189343c94f2b028edaeb
Mezni <mamezni@gmail.com>
2025-01-26 01:13:21 +0000

    commit 2025/01/26

--- work/src/main.rs
+++ work/src/main.rs
@@ -1,20 +1,19 @@
 mod generator;
+use generator::syslog::{SyslogMessage, SyslogMessageBatch};
 
 use arrow::array::StringArray;
 use arrow::datatypes::{DataType, Field, Schema};
 use arrow::record_batch::RecordBatch;
-use generator::syslog::SyslogMessage;
-use generator::syslog::SyslogMessageBatch;
+
+use chrono::Local;
 use parquet::arrow::ArrowWriter;
 use parquet::file::properties::WriterProperties;
-use parquet::file::writer::SerializedFileWriter;
-use chrono::Local;
 use std::fs::File;
 use std::sync::Arc;
+use tokio;
 
-// Assuming that you have SyslogMessage defined and it has the same fields as mentioned previously
+// Converts Vec<SyslogMessage> into RecordBatch
 pub fn vec_to_arrow(messages: Vec<SyslogMessage>) -> RecordBatch {
-    // Convert Vec<SyslogMessage> into columns for Arrow format
     let session_ids: Vec<String> = messages.iter().map(|msg| msg.session_id.clone()).collect();
     let source_ips: Vec<String> = messages
         .iter()
@@ -31,18 +30,16 @@ pub fn vec_to_arrow(messages: Vec<SyslogMessage>) -> RecordBatch {
     let durations: Vec<String> = messages.iter().map(|msg| msg.duration.clone()).collect();
     let msg_types: Vec<String> = messages.iter().map(|msg| msg.msg_type.clone()).collect();
 
-    // Create Arrow Arrays
-    let session_id_array = StringArray::from(session_ids);
-    let source_ip_array = StringArray::from(source_ips);
-    let source_port_array = StringArray::from(source_ports);
-    let dest_ip_array = StringArray::from(dest_ips);
-    let dest_port_array = StringArray::from(dest_ports);
-    let start_ts_array = StringArray::from(start_ts);
-    let end_ts_array = StringArray::from(end_ts);
-    let duration_array = StringArray::from(durations);
-    let msg_type_array = StringArray::from(msg_types);
+    let session_id_array = Arc::new(StringArray::from(session_ids));
+    let source_ip_array = Arc::new(StringArray::from(source_ips));
+    let source_port_array = Arc::new(StringArray::from(source_ports));
+    let dest_ip_array = Arc::new(StringArray::from(dest_ips));
+    let dest_port_array = Arc::new(StringArray::from(dest_ports));
+    let start_ts_array = Arc::new(StringArray::from(start_ts));
+    let end_ts_array = Arc::new(StringArray::from(end_ts));
+    let duration_array = Arc::new(StringArray::from(durations));
+    let msg_type_array = Arc::new(StringArray::from(msg_types));
 
-    // Create Arrow Schema
     let schema = Arc::new(Schema::new(vec![
         Field::new("session_id", DataType::Utf8, false),
         Field::new("source_ip_address", DataType::Utf8, false),
@@ -55,48 +52,37 @@ pub fn vec_to_arrow(messages: Vec<SyslogMessage>) -> RecordBatch {
         Field::new("msg_type", DataType::Utf8, false),
     ]));
 
-    // Create RecordBatch
     RecordBatch::try_new(
         schema,
         vec![
-            Arc::new(session_id_array) as Arc<dyn arrow::array::Array>,
-            Arc::new(source_ip_array),
-            Arc::new(source_port_array),
-            Arc::new(dest_ip_array),
-            Arc::new(dest_port_array),
-            Arc::new(start_ts_array),
-            Arc::new(end_ts_array),
-            Arc::new(duration_array),
-            Arc::new(msg_type_array),
+            session_id_array,
+            source_ip_array,
+            source_port_array,
+            dest_ip_array,
+            dest_port_array,
+            start_ts_array,
+            end_ts_array,
+            duration_array,
+            msg_type_array,
         ],
     )
     .expect("Failed to create RecordBatch")
 }
 
-// Function to save RecordBatch to Parquet file
-pub fn save_to_parquet(record_batch: &RecordBatch, file_path: &str) {
-    // Create a file for writing Parquet data
-    let file = File::create(file_path).expect("Unable to create file");
-
-    // Set writer properties (optional)
+// Saves RecordBatch to Parquet file
+pub fn save_to_parquet(record_batch: &RecordBatch, file_path: &str) -> Result<(), std::io::Error> {
+    let file = File::create(file_path)?;
     let properties = WriterProperties::builder().build();
+    let mut writer = ArrowWriter::try_new(file, record_batch.schema(), Some(properties))?;
 
-    // Create the Parquet Arrow Writer
-    let mut writer = ArrowWriter::try_new(file, record_batch.schema(), Some(properties))
-        .expect("Unable to create Arrow writer");
-
-    // Write the record batch to the Parquet file
-    writer
-        .write(&record_batch)
-        .expect("Unable to write record batch to Parquet");
-
-    // Finish the writer
-    writer.close().expect("Unable to close Parquet writer");
+    writer.write(record_batch)?;
+    writer.close()?;
 
     println!("Parquet file saved to {}", file_path);
+    Ok(())
 }
 
-pub fn generate() {
+pub fn generate() -> Result<(), std::io::Error> {
     for i in 0..5 {
         let mut batch = SyslogMessageBatch::new();
         let messages = batch.generate();
@@ -105,10 +91,14 @@ pub fn generate() {
         let timestamp = Local::now().format("%Y%m%d%H%M%S").to_string();
         let file_path = format!("MQ/INPUT/syslog_{}_0{}.parquet", timestamp, i + 1);
 
-        save_to_parquet(&record_batch, &file_path);
-    }    
+        save_to_parquet(&record_batch, &file_path)?;
+    }
+    Ok(())
 }
 
-fn main() {
-    generate()
+#[tokio::main]
+async fn main() {
+    if let Err(err) = generate() {
+        eprintln!("Error generating Parquet files: {}", err);
+    }
 }
