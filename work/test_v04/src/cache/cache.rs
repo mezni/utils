abb79c87306ffe3d26ad529414cb19bf1e9791ab
Mezni <mamezni@gmail.com>
2025-01-27 01:11:33 +0000

    commit 2025/01/27

--- work/test_v04/src/cache/cache.rs
+++ work/test_v04/src/cache/cache.rs
@@ -1,43 +1,47 @@
-mod cache;
-
-use cache::CacheManager;
-use tokio::runtime::Builder;
-use serde_json;
+use sled::{Db, Tree};
+use std::path::Path;
 use std::collections::HashMap;
 
-fn main() -> Result<(), sled::Error> {
-    let mut runtime = Builder::new_multi_thread().enable_all().build()?;
-    runtime.block_on(async {
-        let mut manager = CacheManager::new("my_cache")?;
+pub struct CacheManager {
+    db: Db,
+    tables: HashMap<String, Tree>,
+}
 
-        let user_key = "1";
-        let user_value = "John Doe";
-        manager.insert_async("users", user_key, user_value).await?;
+impl CacheManager {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, sled::Error> {
+        let db = sled::open(path)?;
+        Ok(CacheManager {
+            db,
+            tables: HashMap::new(),
+        })
+    }
 
-        let user_value = manager.get_async("users", user_key).await?.unwrap();
-        println!("User Value: {}", String::from_utf8(user_value).unwrap());
-
-        manager.delete_async("users", user_key).await?;
-
-        let item: HashMap<String, String> = [
-            ("session_id".to_string(), "233".to_string()),
-            ("timestamp".to_string(), "2014".to_string()),
-            ("type".to_string(), "open".to_string())
-        ].iter().cloned().collect();
-
-        let session_key = item.get("session_id").unwrap().as_str();
-
-        match serde_json::to_vec(&item) {
-            Ok(session_value) => {
-                manager.insert_async("sessions", session_key, session_value).await?;
-                let session_value = manager.get_async("sessions", session_key).await?.unwrap();
-                println!("Session Value: {}", String::from_utf8(session_value).unwrap());
-            }
-            Err(err) => {
-                eprintln!("Error serializing item to JSON: {}", err);
-            }
+    pub fn get_or_create_table(&mut self, name: &str) -> Result<Tree, sled::Error> {
+        if let Some(tree) = self.tables.get(name) {
+            return Ok(tree.clone());
         }
+        let tree = self.db.open_tree(name)?;
+        self.tables.insert(name.to_string(), tree.clone());
+        Ok(tree)
+    }
 
+    pub async fn insert_async<K: AsRef<[u8]>, V: AsRef<[u8]>>(&mut self, table: &str, key: K, value: V) -> Result<(), sled::Error> {
+        let tree = self.get_or_create_table(table)?;
+        let value = value.as_ref().to_vec();
+        tree.insert(key, value)?;
         Ok(())
-    })
+    }
+
+    pub async fn get_async<K: AsRef<[u8]>>(&mut self, table: &str, key: K) -> Result<Option<Vec<u8>>, sled::Error> {
+        let tree = self.get_or_create_table(table)?;
+        let value = tree.get(key)?;
+        let value = value.map(|ivec| ivec.to_vec());
+        Ok(value)
+    }
+
+    pub async fn delete_async<K: AsRef<[u8]>>(&mut self, table: &str, key: K) -> Result<(), sled::Error> {
+        let tree = self.get_or_create_table(table)?;
+        tree.remove(key)?;
+        Ok(())
+    }
 }
--- work/test_v04/src/main.rs
+++ work/test_v04/src/main.rs
@@ -1,84 +1,63 @@
-use sled::{Db, Tree};
-use std::path::Path;
+mod cache;
+use cache::cache::CacheManager;
+use tokio;
 use serde_json;
 use std::collections::HashMap;
-use tokio;
+use sled::IVec;
+use std::time::Instant;
 
-pub struct CacheManager {
-    db: Db,
-    tables: HashMap<String, Tree>,
+async fn create_and_insert_items(manager: &mut CacheManager, table_name: &str) -> Result<(), sled::Error> {
+    for i in 1..=1000 {
+        let item = HashMap::from([
+            ("session_id".to_string(), i.to_string()),
+            ("timestamp".to_string(), "2014".to_string()),
+            ("type".to_string(), "open".to_string())
+        ]);
+
+        let session_key = item.get("session_id").unwrap().as_str();
+
+        match serde_json::to_vec(&item) {
+            Ok(session_value) => {
+                manager.insert_async(table_name, session_key, session_value).await?;
+            }
+            Err(err) => {
+                eprintln!("Error serializing item to JSON: {}", err);
+            }
+        }
+    }
+    Ok(())
 }
 
-impl CacheManager {
-    pub fn new<P: AsRef<Path>>(path: P) -> Result<Self, sled::Error> {
-        let db = sled::open(path)?;
-        Ok(CacheManager {
-            db,
-            tables: HashMap::new(),
-        })
-    }
-
-    fn get_or_create_table(&mut self, name: &str) -> Result<Tree, sled::Error> {
-        if let Some(tree) = self.tables.get(name) {
-            return Ok(tree.clone());
-        }
-        let tree = self.db.open_tree(name)?;
-        self.tables.insert(name.to_string(), tree.clone());
-        Ok(tree)
-    }
-
-    async fn insert_async<K: AsRef<[u8]>, V: AsRef<[u8]>>(&mut self, table: &str, key: K, value: V) -> Result<(), sled::Error> {
-        let tree = self.get_or_create_table(table)?;
-        let value = value.as_ref().to_vec();
-        tree.insert(key, value)?;
-        Ok(())
-    }
-
-    async fn get_async<K: AsRef<[u8]>>(&mut self, table: &str, key: K) -> Result<Option<Vec<u8>>, sled::Error> {
-        let tree = self.get_or_create_table(table)?;
-        let value = tree.get(key)?;
-        let value = value.map(|ivec| ivec.to_vec());
-        Ok(value)
-    }
-
-    async fn delete_async<K: AsRef<[u8]>>(&mut self, table: &str, key: K) -> Result<(), sled::Error> {
-        let tree = self.get_or_create_table(table)?;
-        tree.remove(key)?;
-        Ok(())
-    }
+async fn read_all_items(manager: &mut CacheManager, table_name: &str) -> Result<Vec<(String, Vec<u8>)>, sled::Error> {
+    let tree = manager.get_or_create_table(table_name)?;
+    let items: Vec<(Vec<u8>, Vec<u8>)> = tree.iter()
+        .filter_map(|item| item.ok())
+        .collect::<Vec<(IVec, IVec)>>()
+        .into_iter()
+        .map(|(key, value)| (key.to_vec(), value.to_vec()))
+        .collect();
+    let items: Vec<(String, Vec<u8>)> = items.into_iter().map(|(key, value)| (String::from_utf8(key).unwrap(), value)).collect();
+    Ok(items)
 }
 
 #[tokio::main]
 async fn main() -> Result<(), sled::Error> {
+    let start = Instant::now();
     let mut manager = CacheManager::new("my_cache")?;
+    create_and_insert_items(&mut manager, "sessions").await?;
 
-    let user_key = "1";
-    let user_value = "John Doe";
-    manager.insert_async("users", user_key, user_value).await?;
-
-    let user_value = manager.get_async("users", user_key).await?.unwrap();
-    println!("User Value: {}", String::from_utf8(user_value).unwrap());
-
-    manager.delete_async("users", user_key).await?;
-
-    let item: HashMap<String, String> = [
-        ("session_id".to_string(), "233".to_string()),
-        ("timestamp".to_string(), "2014".to_string()),
-        ("type".to_string(), "open".to_string())
-    ].iter().cloned().collect();
-
-    let session_key = item.get("session_id").unwrap().as_str();
-
-    match serde_json::to_vec(&item) {
-        Ok(session_value) => {
-            manager.insert_async("sessions", session_key, session_value).await?;
-            let session_value = manager.get_async("sessions", session_key).await?.unwrap();
-            println!("Session Value: {}", String::from_utf8(session_value).unwrap());
-        }
-        Err(err) => {
-            eprintln!("Error serializing item to JSON: {}", err);
-        }
+    let items = read_all_items(&mut manager, "sessions").await?;
+    for (key, value) in items {
+        let item: HashMap<String, String> = match serde_json::from_slice(&value) {
+            Ok(item) => item,
+            Err(err) => {
+                eprintln!("Error deserializing item from JSON: {}", err);
+                return Ok(());
+            }
+        };
+        println!("Session {}: {:?}", key, item);
     }
-
+    let duration = start.elapsed();
+    println!("Time taken to read all items: {:?}", duration);
     Ok(())
 }
