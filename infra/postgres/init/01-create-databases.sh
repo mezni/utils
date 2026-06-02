992bdc15e4866f9abc12b43c09a902bb4b18eeed
M.Mezni <mamezni@gmail.com>
2026-06-02 09:28:06 -0400

    fix: create keycloak user in PostgreSQL init script
    Keycloak connects as user 'keycloak' but only default user 'borne'
    was created by the init script.

--- infra/postgres/init/01-create-databases.sh
+++ infra/postgres/init/01-create-databases.sh
@@ -2,8 +2,11 @@
 set -e
 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE keycloak_db;
+    CREATE USER keycloak WITH PASSWORD 'change-me';
+    CREATE DATABASE keycloak_db OWNER keycloak;
     CREATE DATABASE users_db;
     CREATE DATABASE inventory_db;
     CREATE DATABASE analytics_db;
+
+    GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO keycloak;
 EOSQL
