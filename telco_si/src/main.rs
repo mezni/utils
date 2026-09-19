a63ff5091ae745065fc0823c67446cddd251e7fe
M.Mezni <mamezni@gmail.com>
2026-09-19 13:24:10 -0400

    Add web api

--- telco_si/src/main.rs
+++ telco_si/src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+
+async fn health() -> impl Responder {
+    HttpResponse::Ok().json(serde_json::json!({
+        "status": "ok"
+    }))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/health", web::get().to(health))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
